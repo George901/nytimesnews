50408a81bf4ae0dd016b11ef3f5db9e4337d086a
Georgiy Farafonov <>
2020-09-25 11:50:41 +0300

    added news cell

--- nytimesnews/UI/NewsList/Cell/NewsCell.swift
+++ nytimesnews/UI/NewsList/Cell/NewsCell.swift
@@ -8,16 +8,27 @@
 import UIKit
 
 class NewsCell: UITableViewCell {
+    
+    @IBOutlet private weak var newsTitleLabel: UILabel!
+    @IBOutlet private weak var absctractLabel: UILabel!
+    @IBOutlet private weak var authorLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var favoritesButton: UIButton!
+    
+    private var onFavoritePress: ((Bool)->())?
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    func setupWith(news: News, isFavorite: Bool, onFavoritePress: ((Bool)->())?) {
+        newsTitleLabel.text = news.title
+        absctractLabel.text = news.abstract
+        authorLabel.text = news.author
+        dateLabel.text = news.publishedAt
+        favoritesButton.isSelected = isFavorite
+        self.onFavoritePress = onFavoritePress
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    @IBAction private func favoritesButtonPressed(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        onFavoritePress?(sender.isSelected)
     }
-
+    
 }
